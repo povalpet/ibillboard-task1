9de50440c5d9e89d1fc156ba7600016bce14251e
Petr Povala <povalpet@gmail.com>
2014-05-09 00:30:07 +0200

    implemented redis service

--- services/redis/redis.js
+++ services/redis/redis.js
@@ -1,7 +1,27 @@
+var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 module.exports = function(){
   function RedisService(connector) {
     this.connector = connector;
+    this.client = this.createClient();
+    this.increment = __bind(this.increment,this);
   }
+
+  RedisService.prototype.createClient = function(port,host,options) {
+    var client;
+    if (port === null) {
+      port = 6379;
+    }
+    if (host === null) {
+      host = '127.0.0.1';
+    }
+    client = this.connector.createClient(port, host, options);
+
+    return client;
+  };
+
+  RedisService.prototype.increment = function(value, callback) {
+    this.client.send_command('INCRBY',value,callback);
+  };
 
   return RedisService;
 
--- services/redis/redis_test.js
+++ services/redis/redis_test.js
@@ -2,13 +2,55 @@ var RedisService;
 RedisService = require('./redis');
 
 describe('Unit - RedisService',function(){
-  var instance;
+  var instance, redisMock;
 
   beforeEach(function(){
-    instance = new RedisService();
+    redisMock = {};
+    redisMock.createClient = function() {
+      redisClientMock = {};
+      redisClientMock.send_command = spy();
+
+      return redisClientMock;
+    };
+    spy(redisMock,'createClient');
+  });
+
+  beforeEach(function(){
+    instance = new RedisService(redisMock);
   });
 
   it('should be initialized', function(){
     assert.instanceOf(instance,RedisService);
+  });
+
+  it('should create redis client', function() {
+    assert.isObject(instance.client);
+  });
+
+  describe('method createClient', function(){
+    it('should be a function',function(){
+      assert.isFunction(instance.createClient);
+    });
+    it('should call createClient', function() {
+      assert.isTrue(redisMock.createClient.called);
+    });
+  });
+
+  describe('method increment', function(){
+    var callback, value;
+    beforeEach(function(){
+      value = ['count',10];
+      callback = function(){};
+      instance.increment(value,callback);
+    });
+    it('should be a function', function(){
+      assert.isFunction(instance.increment);
+    });
+    it('should call redis client', function(){
+      assert.isTrue(instance.client.send_command.called);
+    });
+    it('should call redis with proper arguments', function(){
+      assert.isTrue(instance.client.send_command.calledWith('INCRBY',value,callback));
+    });
   });
 });
