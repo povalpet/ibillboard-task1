0886ce6eec64d4446a7f861aa83b86e8fa1b0897
Petr Povala <povalpet@gmail.com>
2014-05-08 23:32:58 +0200

    added comments

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ TrackRoute = require('./track');
 
 var create;
 
+/**
+ * Initialize route handlers
+ * @param  {Object} model
+ * @return {Object} created route handlers
+ */
 create = function(model) {
   var track = new TrackRoute(model.redis,model.track);
   return {
@@ -12,6 +17,11 @@ create = function(model) {
 };
 
 var registerRoutes;
+/**
+ * Registering app routes
+ * @param  {Object} app    express instance
+ * @param  {Object} routes route handlers
+ */
 registerRoutes = function(app, routes) {
   app.get('/track',routes.track.handleGet);
 };
